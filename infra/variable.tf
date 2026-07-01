ff065afb19b350cf2b55ac1fa46a44040883f2e9
iali-11 <ibrahima_11@outlook.com>
2026-07-01 22:36:28 +0100

    feat: introduced list of strings for resources with count argument

--- infra/variable.tf
+++ infra/variable.tf
@@ -3,34 +3,19 @@ variable "vpc_cidr_block" {
   default = "10.0.0.0/22"
 }
 
-variable "public_subnet_1_cidr_block" {
-  type    = string
-  default = "10.0.0.0/25"
+variable "public_subnet_cidr_blocks" {
+  type    = list(string)
+  default = ["10.0.0.0/25", "10.0.1.0/25"]
 }
 
-variable "public_subnet_2_cidr_block" {
-  type    = string
-  default = "10.0.1.0/25"
+variable "private_subnet_cidr_blocks" {
+  type    = list(string)
+  default = ["10.0.2.0/25", "10.0.3.0/25"]
 }
 
-variable "private_subnet_1_cidr_block" {
-  type    = string
-  default = "10.0.2.0/25"
-}
-
-variable "private_subnet_2_cidr_block" {
-  type    = string
-  default = "10.0.3.0/25"
-}
-
-variable "availability_zone_1" {
-  type    = string
-  default = "eu-west-2a"
-}
-
-variable "availability_zone_2" {
-  type    = string
-  default = "eu-west-2b"
+variable "availability_zones" {
+  type    = list(string)
+  default = ["eu-west-2a", "eu-west-2b"]
 }
 
 variable "default_cidr_block" {
