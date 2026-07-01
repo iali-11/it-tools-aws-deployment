dc0e389c9096299677c770a749f08d1cf1ee87a8
iali-11 <ibrahima_11@outlook.com>
2026-07-01 17:45:34 +0100

    feat: acm cert variable values

---
+++ infra/modules/acm/variable.tf
@@ -0,0 +1,19 @@
+variable "it_tools_alb" {
+  type = string
+}
+
+variable "it_tools_alb_dns_name" {
+  type = string
+}
+
+variable "it_tools_alb_zone_id" {
+  type = string
+}
+
+variable "it_tools_domain_name_root" {
+  type = string
+}
+
+variable "it_tools_domain_name_www" {
+  type = string
+}
